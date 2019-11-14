78ebdeb2c3d8f8536c760e5f0004eb897c4cb105
farhan <farhansadikgalib@gmail.com>
2019-11-15 00:41:16 +0600

    it has done its splash screen and share button.. also it works as animation in dingdong round button.

--- app/src/main/java/com/farhan/dingdong/MainActivity.java
+++ app/src/main/java/com/farhan/dingdong/MainActivity.java
@@ -1,63 +1,120 @@
 package com.farhan.dingdong;
 
-import android.app.Activity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.Random;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
+   private Button button;
+   private MediaPlayer player;
+    AnimationDrawable animationDrawable;
 
-    int[] sounds={R.raw.xp_error_mix, R.raw.yeahyeah, R.raw.toads, R.raw.dingdong};
-
-    private Button btn;
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
 
-         btn =  findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        button = findViewById( R.id.btn );
 
+        ImageView img = findViewById(R.id.image);
+        img.setBackgroundResource(R.drawable.anim);
+        animationDrawable = (AnimationDrawable) img.getBackground();
+
+        final int[] sound= {R.raw.xp_error_mix,R.raw.yeahyeah,R.raw.dingdong,R.raw.dingdong};
+
+        button.setOnTouchListener( new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                Random r = new Random();
-                int Low = 0;
-                int High = 4;
-                int rndm = r.nextInt(High-Low)/1+1;
-                final MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), sounds[rndm]);
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.pause();
-                        mp.stop();
-                        mp.release();
-                    }
-                });
-                mediaPlayer.start();
+            public boolean onTouch(View v, MotionEvent event) {
 
+
+                if (event.getAction()== MotionEvent.ACTION_DOWN) {
+                    animationDrawable.start();
+
+                    Random random = new Random();
+                    int low=0;
+                    int high=3;
+                    int rnd = random.nextInt(high-low)+low;
+
+                    player= MediaPlayer.create( MainActivity.this,sound[rnd] );
+                    player.start();
+
+                    v.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                    v.invalidate();
+                }
+                else if(event.getAction()==MotionEvent.ACTION_UP){
+                    animationDrawable.stop();
+                    animationDrawable.selectDrawable(0);
+                    player.stop();
+                    player.release();
+
+                    v.getBackground().clearColorFilter();
+                    v.invalidate();
+                }
+                return true;
 
             }
-        });
+
+        } );
 
 
+    }
 
+ @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-
-        //final MediaPlayer mp = MediaPlayer.create(this, R.raw.blackberry.mp3);
-
-        //Button play_button = (Button)this.findViewById(R.id.play_button);
-        //play_button.setOnClickListener(new View.OnClickListener() {
-        // public void onClick(View v) {
-
-        // mp.start();
-        //}
-        // });
-
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
     }
 
 
 
+
+
+    @Override
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+
+        switch (item.getItemId()) {
+
+            case R.id.shareme:
+
+                Intent i = new Intent(
+                        android.content.Intent.ACTION_SEND);
+
+                i.setType("text/plain");
+
+                startActivity(Intent.createChooser(i, "Share Via"));
+
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
+
