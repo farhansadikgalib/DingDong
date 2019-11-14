72d8d4a92adb2af356d475b3cb16a8a76004d8e9
farhan <farhansadikgalib@gmail.com>
2019-11-14 06:20:39 +0600

    splash screen is done also here i used progressbar for loading

--- app/src/main/java/com/farhan/dingdong/MainActivity.java
+++ app/src/main/java/com/farhan/dingdong/MainActivity.java
@@ -1,63 +1,31 @@
 package com.farhan.dingdong;
 
-import android.app.Activity;
-import android.media.MediaPlayer;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import java.util.Random;
-
-public class MainActivity extends Activity {
-
-
-    int[] sounds={R.raw.xp_error_mix, R.raw.yeahyeah, R.raw.toads, R.raw.dingdong};
-
-    private Button btn;
+public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         btn =  findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Random r = new Random();
-                int Low = 0;
-                int High = 4;
-                int rndm = r.nextInt(High-Low)/1+1;
-                final MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), sounds[rndm]);
-                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.pause();
-                        mp.stop();
-                        mp.release();
-                    }
-                });
-                mediaPlayer.start();
-
-
-            }
-        });
-
-
-
-
-
-        //final MediaPlayer mp = MediaPlayer.create(this, R.raw.blackberry.mp3);
-
-        //Button play_button = (Button)this.findViewById(R.id.play_button);
-        //play_button.setOnClickListener(new View.OnClickListener() {
-        // public void onClick(View v) {
-
-        // mp.start();
-        //}
-        // });
-
+        progressBar = findViewById(R.id.progressbar);
+        textView = findViewById(R.id.textView);
+        progressBar.setMax(100);
+        progressBar.setScaleX(1f);
+        progressAnimation();
     }
 
+    public void progressAnimation(){
 
+        ProgressbarAnimation progressbarAnimation = new ProgressbarAnimation(this,progressBar,textView,0f,100f);
+        progressbarAnimation.setDuration(5000);
+        progressBar.setAnimation(progressbarAnimation);
+    }
 
 }
